3b42c9ef9d8881ec7d63e457351e295900928a42
adithyahosapate <adihosapate@gmail.com>
2017-10-02 15:30:33 +0530

    added files

--- 4to1multiplexer/Gate_level/mux.v
+++ 4to1multiplexer/Gate_level/mux.v
@@ -8,6 +8,24 @@ wire      q;
 wire[1:0] select;
 wire[3:0] d;
 
-assign q = d[select];
+
+wire a_1,a_2,a_3,a_4,a_5,a_6,a_7,a_8,temp_1,temp_2;
+
+and a1(a_1,d[0],!select[1]);
+and a2(a_2,a_1,!select[0]);
+
+and a3(a_3,d[1],!select[1]);
+and a4(a_4,a_3,select[0]);
+
+and a5(a_5,d[2],select[1]);
+and a6(a_6,a_5,!select[0]);
+
+and a7(a_7,d[3],select[1]);
+and a8(a_8,a_7,select[0]);
+
+or o1(temp_1,a_8,a_6);
+or o2(temp_2,a_4,a_2);
+
+or o3(q,temp_2,temp_1);
 
 endmodule
--- full_adder/Gate_level/fulladder.v
+++ full_adder/Gate_level/fulladder.v
@@ -11,9 +11,8 @@ and a1(and_1,xor_1,c_in);
 xor x2(sum,xor_1,c_in);
 and a2(and_2,in0,in1);
 
-or o1(or_1,and_2,and_1);
+or o1(c_out,and_2,and_1);
 
-assign c_out=or_1;
-assign sum=xor_2;
+
 
 endmodule
